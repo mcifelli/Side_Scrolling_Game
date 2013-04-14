c295223e92ef4fd92d5d3f9988d38907cd2f86fb
mcifelli <mcifelli@ycp.edu>
2013-04-13 20:56:12 -0400

    Completed alpha viewer for map values

--- src/dtran/mcifelli/game/client/GameViewAlpha.java
+++ src/dtran/mcifelli/game/client/GameViewAlpha.java
@@ -1,29 +1,32 @@
 package dtran.mcifelli.game.client;
 
+import dtran.mcifelli.game.map.Terrain;
 import dtran.mcifelli.game.objects.Game;
 
 public class GameViewAlpha {
-	private Game model;
+	private static Game model;
 	
 	public static void main(String[] args) {
 		model = new Game();
 		buildMap();
 		displayMap();
-		
-		
 	}
 	
 	private static void displayMap() {
 		for (int row = 0; row < model.getHeight(); row ++) {
 			for (int col = 0; col < model.getWidth(); col++) {
-				
-				
+				System.out.print(model.getMap()[row][col].toString());
 			}
+			System.out.print("\n");
 		}
 	}
 
 	private static void buildMap() {
-		
+		for (int row = 0; row < model.getHeight(); row ++) {
+			for (int col = 0; col < model.getWidth(); col++) {
+				model.setTerrainAtLocation(row, col, Terrain.grass);
+			}
+		}
+		model.setTerrainAtLocation(0, 0, Terrain.air);
 	}
-	
 }
--- src/dtran/mcifelli/game/objects/Game.java
+++ src/dtran/mcifelli/game/objects/Game.java
@@ -13,20 +13,30 @@ public class Game {
 	// CONSTRUCTOR
 	public Game() {
 		
-		
-		
-		
-		
-		
-		
-		
-		
 	}
-
+	
+	// METHODS
+	public boolean setTerrainAtLocation(int row, int col, Terrain terrain) {
+		if ((0 <= row && row < HEIGHT) && (0 <= col && col < WIDTH)) {
+			map[row][col] = terrain;
+			return true;
+		}
+		return false;
+	}
+	
+	
+	
+	// GETTERS & SETTERS
 	public Terrain[][] getMap() {
 		return map;
 	}
 	public void setMap(Terrain[][] map) {
 		this.map = map;
+	}
+	public int getHeight() {
+		return HEIGHT;
+	}
+	public int getWidth() {
+		return 	WIDTH;
 	}
 }
